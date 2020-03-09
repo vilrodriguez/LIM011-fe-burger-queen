e4b29c4808dac64cd65a14f736eed6691aa3d619
Vil Rodriguez <vilmango@hotmail.com>
2020-03-09 17:23:50 -0500

    update menu compnent and its services, showing data base content array in component using menu buttoms

--- burger-queen-App/src/app/components/menu-breakfast/menu-breakfast.component.ts
+++ burger-queen-App/src/app/components/menu-breakfast/menu-breakfast.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { FirestoreService } from "../../services/firestore.service";
+import { MenuNameService } from 'src/app/services/menu-name-service.service';
 
 @Component({
   selector: "app-menu-breakfast",
@@ -8,13 +9,35 @@ import { FirestoreService } from "../../services/firestore.service";
   styleUrls: ["./menu-breakfast.component.scss"]
 })
 export class MenuBreakfastComponent implements OnInit {
+  menuString: string;
   public menuItems = [];
-  constructor(private db: FirestoreService) {
-    /*    this.db.getDataByCategory()
+  constructor(private db: FirestoreService, private menuNameService: MenuNameService) {
+    this.menuNameService.currentString.subscribe(string =>{
+      this.menuString = string;
+      console.log(this.menuString);
+      
+      this.db.getDataByCategory(this.menuString)
     .subscribe((data) => {
-      console.log(data)
-    }) */
+      this.menuItems = data;
+      console.log(data)});
+    });
   }
 
   ngOnInit() {}
 }
+
+// export class MenuBreakfastComponent implements OnInit {
+//   menuString: string;
+//   public menuItems = [];
+//   constructor(private db: FirestoreService, private menuNameService: MenuNameService) {
+//     this.menuNameService.currentString.subscribe(string =>{
+//       this.menuString = string;
+//     });
+//     /*    this.db.getDataByCategory(menuName)
+//     .subscribe((data) => {
+//       console.log(data)
+//     }) */
+//   }
+
+//   ngOnInit() {}
+// }
