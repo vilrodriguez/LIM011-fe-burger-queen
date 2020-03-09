7a44f60c5bf04df2b875986e405dd9bbecb3fb76
Gabby97 <gabbacvdo@gmail.com>
2020-03-09 17:54:15 -0500

    clean commented code

--- burger-queen-App/src/app/components/menu-breakfast/menu-breakfast.component.ts
+++ burger-queen-App/src/app/components/menu-breakfast/menu-breakfast.component.ts
@@ -14,30 +14,11 @@ export class MenuBreakfastComponent implements OnInit {
   constructor(private db: FirestoreService, private menuNameService: MenuNameService) {
     this.menuNameService.currentString.subscribe(string =>{
       this.menuString = string;
-      console.log(this.menuString);
-      
+
       this.db.getDataByCategory(this.menuString)
     .subscribe((data) => {
-      this.menuItems = data;
-      console.log(data)});
+      return this.menuItems = data;
     });
   }
-
   ngOnInit() {}
-}
-
-// export class MenuBreakfastComponent implements OnInit {
-//   menuString: string;
-//   public menuItems = [];
-//   constructor(private db: FirestoreService, private menuNameService: MenuNameService) {
-//     this.menuNameService.currentString.subscribe(string =>{
-//       this.menuString = string;
-//     });
-//     /*    this.db.getDataByCategory(menuName)
-//     .subscribe((data) => {
-//       console.log(data)
-//     }) */
-//   }
-
-//   ngOnInit() {}
-// }
+ }
