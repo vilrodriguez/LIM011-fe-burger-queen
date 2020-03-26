eb81baf60d0d096cc72436e57056fb92d279d920
GlisseLisbeth <vilmango@hotmail.com>
2020-03-26 15:45:48 -0500

    updated total function in component

--- burger-queen-App/src/app/components/customer-request/customer-request.component.ts
+++ burger-queen-App/src/app/components/customer-request/customer-request.component.ts
@@ -19,6 +19,10 @@ export class CustomerRequestComponent implements OnInit {
 
     });
   }
+  
+  getTotal(result) {
+    return result.reduce((a,b) =>a+b.subtotal,0);
+  }
 
 order(obj) {
     this.result = obj;
