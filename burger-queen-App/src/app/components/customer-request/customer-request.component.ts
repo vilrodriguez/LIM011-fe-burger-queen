e9707bc9bd2886126e4fa79b375c3b44b6d64113
Gabby97 <gabbacvdo@gmail.com>
2020-04-01 21:11:38 -0500

    cancel orden

--- burger-queen-App/src/app/components/customer-request/customer-request.component.ts
+++ burger-queen-App/src/app/components/customer-request/customer-request.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuNameService } from 'src/app/services/menu-name-service.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { FirestoreService } from '../../services/firestore.service';
-
+import { Component, OnInit } from "@angular/core";
+import { MenuNameService } from "src/app/services/menu-name-service.service";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FirestoreService } from "../../services/firestore.service";
 
 @Component({
-  selector: 'app-customer-request',
-  templateUrl: './customer-request.component.html',
-  styleUrls: ['./customer-request.component.scss']
+  selector: "app-customer-request",
+  templateUrl: "./customer-request.component.html",
+  styleUrls: ["./customer-request.component.scss"],
 })
 export class CustomerRequestComponent implements OnInit {
   // checkBoxAlertDelivery:string = 'PARA LLEVAR';
@@ -16,64 +15,94 @@ export class CustomerRequestComponent implements OnInit {
   result: any;
   currentproduct: any;
   orderedItem: any;
-  customerName = '';
-  constructor(private menuNameService: MenuNameService,
-              private builder: FormBuilder,
-              private dataBase: FirestoreService) {
-
+  customerName = "";
+  constructor(
+    private menuNameService: MenuNameService,
+    private builder: FormBuilder,
+    private dataBase: FirestoreService
+  ) {
     this.OrderForm = builder.group({
-      customerName: [''] ,
+      customerName: [""],
       table: [0],
       delivery: false,
     });
 
-    this.menuNameService.currentProduct.subscribe(obj => {
+    this.menuNameService.currentProduct.subscribe((obj) => {
       this.currentproduct = obj;
       this.order(this.currentproduct);
     });
   }
 
   getTotal(result: any[]) {
-    return result.reduce(( iterator: number, element: { subtotal: number; }) => {
+    return result.reduce((iterator: number, element: { subtotal: number }) => {
       return iterator + element.subtotal;
     }, 0);
   }
 
   order(obj: {}) {
-      this.result = obj;
-      this.result.forEach(( element: {} ) => this.orderedItem = element);
+    this.result = obj;
+    this.result.forEach((element: {}) => (this.orderedItem = element));
   }
-  reduceProduct(item: { product: string; price: number; quantity: number; subtotal: number; }) {
-    const newObj = {product: item.product, price: item.price, quantity: item.quantity, subtotal: item.subtotal};
-      // create new obj with all element + quantity
+  reduceProduct(item: {
+    product: string;
+    price: number;
+    quantity: number;
+    subtotal: number;
+  }) {
+    const newObj = {
+      product: item.product,
+      price: item.price,
+      quantity: item.quantity,
+      subtotal: item.subtotal,
+    };
+    // create new obj with all element + quantity
     return this.menuNameService.reduceProductOrder(newObj);
   }
 
-  deleteProduct(item: { product: string; price: number; quantity: number; subtotal: number; }) {
-      const newObj = {product: item.product, price: item.price, quantity: item.quantity, subtotal: item.subtotal};
-      // create new obj with all element + quantity
-      return this.menuNameService.deleteProductOrder(newObj);
+  deleteProduct(item: {
+    product: string;
+    price: number;
+    quantity: number;
+    subtotal: number;
+  }) {
+    const newObj = {
+      product: item.product,
+      price: item.price,
+      quantity: item.quantity,
+      subtotal: item.subtotal,
+    };
+    // create new obj with all element + quantity
+    return this.menuNameService.deleteProductOrder(newObj);
   }
   // getCustomerName(event: Event) {
   //   this.customerName = ( event.target as HTMLInputElement).value;
   // }
-  sendOrder(values: { customerName: string; table: number; delivery: boolean; }, result: any, todaydate: any) {
-      const finalOrder = {
+  sendOrder(
+    values: { customerName: string; table: number; delivery: boolean },
+    result: any,
+    todaydate: any
+  ) {
+    const finalOrder = {
       customerName: values.customerName,
       tableNumber: values.table,
       delivery: values.delivery,
       date: todaydate,
-      order: result
-      };
-      // console.log(finalOrder);
-      this.dataBase.sendOrderToKitchen(finalOrder);
-      console.log('sent to kitchen');
-      this.menuNameService.resetOrder();
-      this.OrderForm.reset();
-
+      order: result,
+    };
+    // console.log(finalOrder);
+    this.dataBase.sendOrderToKitchen(finalOrder);
+    console.log("sent to kitchen");
+    this.menuNameService.resetOrder();
+    this.OrderForm.reset();
   }
-ngOnInit() {
-  this.todaydate = this.menuNameService.todayDate();
-  // this.sendOrder(this.OrderForm.value, this.result, this.todaydate);
+  cancelOrder() {
+    this.menuNameService.resetOrder();
+    this.OrderForm.reset();
+    console.log("Cancelled Order");
+  }
+
+  ngOnInit() {
+    this.todaydate = this.menuNameService.todayDate();
+    // this.sendOrder(this.OrderForm.value, this.result, this.todaydate);
   }
 }
