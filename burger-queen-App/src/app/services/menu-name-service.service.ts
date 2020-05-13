7968ad619334605f929222714d857ba3e6f2fef0
vilrodriguez <vilmango@hotmail.com>
2020-05-12 22:55:45 -0500

    fixed conflicts

--- burger-queen-App/src/app/services/menu-name-service.service.ts
+++ burger-queen-App/src/app/services/menu-name-service.service.ts
@@ -10,19 +10,10 @@ export class MenuNameService {
   private arrOrder = new BehaviorSubject([]);
   currentString = this.menuNameSource.asObservable();
   currentProduct = this.arrOrder.asObservable();
-<<<<<<< HEAD
-=======
-
->>>>>>> bd453660d1cc3ca210de285252d7fb6eee95f6ff
   changeString(value: string) {
     this.menuNameSource.next(value);
   }
-  todayDate() {
-    const ndate = new Date();
-    // const d = ndate.getDate() + ndate.getHours() + ndate.getMonth();
-    // return d;
-    return ndate;
-  }
+
   changeProduct(obj: { product: string; price: number; image: string; quantity: number; subtotal: number; }) {
     let newArrOrder: any;
     const findProduct = this.arrOrder.value.find(element => element.product === obj.product);
